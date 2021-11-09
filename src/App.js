344aafa1670f888aac1a09e48b41d60c9785a5b6
MortonaPreguica <raul31fish@gmail.com>
2021-11-08 23:28:02 -0300

    Tabuleiro Consertos

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import {useState, useEffect} from 'react'
 
 const width = 8
-const colors = [
+const candyColors = [
   'blue',
   'green',
   'orange',
@@ -12,31 +12,52 @@ const colors = [
 const App = () => {
   const [currentColorArrangement, setCurrentColorArrangement] = useState([])
 
-  const checkForColumnOfThree = () => {
-    for(let i = 0; i < 47; i++) {
-      const columnOfThree = [i, i + width, i + width * 2]
-      const decidedColor = currentColorArrangement[i]
-      
-      if(columnOfThree.every(square => currentColorArrangement[square] === decidedColor)) {
-        columnOfThree.forEach(square => currentColorArrangement[square] = '')
-      }
-    }
-  }
-
   const checkForColumnOfFour = () => {
     for (let i = 0; i <= 39; i++) {
         const columnOfFour = [i, i + width, i + width * 2, i + width * 3]
         const decidedColor = currentColorArrangement[i]
         const isBlank = currentColorArrangement[i] === blank
 
-        if (columnOfFour.every(square => currentColorArrangement[square] === decidedColor)) {
-            columnOfFour.forEach(square => currentColorArrangement[square] = '')
-          
+        if (columnOfFour.every(square => currentColorArrangement[square] === decidedColor && !isBlank)) {
+            setScoreDisplay((score) => score + 4)
+            columnOfFour.forEach(square => currentColorArrangement[square] = blank)
+            return true
         }
     }
-  }
+}
 
-  const checkForRowOfThree = () => {
+const checkForRowOfFour = () => {
+    for (let i = 0; i < 64; i++) {
+        const rowOfFour = [i, i + 1, i + 2, i + 3]
+        const decidedColor = currentColorArrangement[i]
+        const notValid = [5, 6, 7, 13, 14, 15, 21, 22, 23, 29, 30, 31, 37, 38, 39, 45, 46, 47, 53, 54, 55, 62, 63, 64]
+        const isBlank = currentColorArrangement[i] === blank
+
+        if (notValid.includes(i)) continue
+
+        if (rowOfFour.every(square => currentColorArrangement[square] === decidedColor && !isBlank)) {
+            setScoreDisplay((score) => score + 4)
+            rowOfFour.forEach(square => currentColorArrangement[square] = blank)
+            return true
+        }
+    }
+}
+
+const checkForColumnOfThree = () => {
+    for (let i = 0; i <= 47; i++) {
+        const columnOfThree = [i, i + width, i + width * 2]
+        const decidedColor = currentColorArrangement[i]
+        const isBlank = currentColorArrangement[i] === blank
+
+        if (columnOfThree.every(square => currentColorArrangement[square] === decidedColor && !isBlank)) {
+            setScoreDisplay((score) => score + 3)
+            columnOfThree.forEach(square => currentColorArrangement[square] = blank)
+            return true
+        }
+    }
+}
+
+const checkForRowOfThree = () => {
     for (let i = 0; i < 64; i++) {
         const rowOfThree = [i, i + 1, i + 2]
         const decidedColor = currentColorArrangement[i]
@@ -52,27 +73,50 @@ const App = () => {
         }
     }
 }
-
-  const createBoard = () => {
-    const randomColorArrangement = []
-    for(let i = 0; i < width * width; i++) {
-      const randomColor = colors[Math.floor(Math.random() * colors.length)]
-      randomColorArrangement.push(randomColor)
-    }
-    setCurrentColorArrangement(randomColorArrangement)
-  }
   
+const moveIntoBelow = () => {
+  for (let i = 0; i < 65; i++) {
+    const firstRow = [0, 1, 2, 3, 4, 5, 6, 7]
+    const isFirst = firstRow.includes(i) 
+
+    // Adicionando valores para a primeira fileira
+    if(isFirst && currentColorArrangement === '') { 
+      let randomNumber = Math.floor(Math.random() * candyColors.length)
+      currentColorArrangement[i] = candyColors[randomNumber]
+    }
+
+    // Desecendo os valores para a fileira de baixo caso esteja vazia
+    if(currentColorArrangement[i + width] === '') {
+      currentColorArrangement[i + width] = currentColorArrangement[i]
+      currentColorArrangement[i] = ''
+    }
+  }
+}
+
+const createBoard = () => {
+  const randomColorArrangement = []
+  for (let i = 0; i < width * width; i++) {
+      const randomColor = candyColors[Math.floor(Math.random() * candyColors.length)]
+      randomColorArrangement.push(randomColor)
+  }
+  setCurrentColorArrangement(randomColorArrangement)
+}
+
   useEffect(() => {
     createBoard()
   }, [])
 
   useEffect(() => {
     const timer = setInterval(() => {
-      checkForColumnOfThree()
-      setCurrentColorArrangement([...currentColorArrangement])
+        checkForColumnOfFour()
+        checkForRowOfFour()
+        checkForColumnOfThree()
+        checkForRowOfThree()
+        moveIntoSquareBelow()
+        setCurrentColorArrangement([...currentColorArrangement])
     }, 100)
     return () => clearInterval(timer)
-  }, [checkForColumnOfThree, currentColorArrangement])
+}, [checkForColumnOfFour, checkForRowOfFour, checkForColumnOfThree, checkForRowOfThree, moveIntoSquareBelow, currentColorArrangement])
 
   return (
     <div className='app'>
